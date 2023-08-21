4989e68f987b4e1870e2656317f951e709ada4f5
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-08-21 16:53:43 +0530

    Add files via upload

---
+++ Line_Fitting/Q2_vis.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('equations.txt', 'r') as f:
+    equations = f.readlines()
+
+for equation in equations:
+    x = np.linspace(-5, 10, 2)
+    y = eval(equation)
+    plt.plot(x, y, label=equation)
+
+with open('points.txt', 'r') as f:
+    points = f.readlines()
+
+for point in points:
+    x, y = point.split()
+    plt.plot(x, y, 'ro')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Line Fitting')
+plt.legend()
+plt.show()
+
